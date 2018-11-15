83dfcedf0259247d4217ab10cd7db2763eeddc0c
Stephen Iskander <siskande@oswego.edu>
2018-11-15 13:28:05 -0500

    initial commit

---
+++ src/main/java/edu/oswego/csc366/blocksworld/App.java
@@ -0,0 +1,13 @@
+package edu.oswego.csc366.blocksworld;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
