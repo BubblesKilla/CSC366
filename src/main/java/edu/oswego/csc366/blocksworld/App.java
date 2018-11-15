61cb5202b44d205a668727558dceeb7d0589af7c
Stephen Iskander <siskande@oswego.edu>
2018-11-15 13:57:30 -0500

    initialize block configuration

--- src/main/java/edu/oswego/csc366/blocksworld/App.java
+++ src/main/java/edu/oswego/csc366/blocksworld/App.java
@@ -1,13 +1,41 @@
 package edu.oswego.csc366.blocksworld;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.util.List;
+import java.util.Deque;
+import java.util.ArrayList;
+import java.util.ArrayDeque;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class App {
+    static final int STACK_COUNT = 4;
+    static final int BLOCK_COUNT = 6;
+    static List<Deque<Character>> stacks;
+
+    public static void main( String[] args ) {
+        var blocks = generateBlocks();
+        var stacks = initialConfig(blocks);
+    }
+
+    private static List<Character> generateBlocks() {
+        var blocks = new ArrayList<Character>();
+        char current = 'A';
+        for(int i = 0; i < BLOCK_COUNT; i++)
+            blocks.add(current++);
+        return blocks;
+    }
+
+    private static ArrayList<ArrayDeque<Character>> initialConfig(List<Character> blocks) {
+        var stacks = new ArrayList<ArrayDeque<Character>>();
+
+        for(int i = 0; i < STACK_COUNT; i++)
+            stacks.add(new ArrayDeque<>());
+
+        while(blocks.size() > 1)
+            stacks.get(ThreadLocalRandom.current().nextInt(stacks.size()))
+                    .add(blocks.remove(ThreadLocalRandom.current().nextInt(blocks.size())));
+        stacks.get(ThreadLocalRandom.current().nextInt(stacks.size()))
+                .add(blocks.get(0));
+
+        return stacks;
     }
 }
